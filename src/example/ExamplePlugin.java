def3b95262673b6a469b15ae4a31a4181cf42912
acrobat135 <83962234+acrobat135@users.noreply.github.com>
2021-05-10 17:34:18 +0300

    Change format

--- src/example/ExamplePlugin.java
+++ src/example/ExamplePlugin.java
@@ -16,9 +16,9 @@ public class ExamplePlugin extends Plugin{
 
         Events.on(EventType.PlayerChatEvent.class, event -> {
             if (!event.message.startsWith("/")) {
-                String prefix = event.player.admin() ? "[[red]АДМИН[]]" : "[ИГРОК]";
+                String prefix = event.player.admin() ? "[админ]" : "[игрок]";
                 String playerName = NetClient.colorizeName(event.player.id, event.player.name);
-                Call.sendMessage(prefix + playerName + event.message);
+                Call.sendMessage(prefix + playerName + "[white]> " + event.message);
             }
         });
 
@@ -37,20 +37,20 @@ public class ExamplePlugin extends Plugin{
             }
 
             String message = args[0];
-            String prefix = "[[red]АДМИН[]]";
+            String prefix = "[админ]";
             String playerName = NetClient.colorizeName(player.id, player.name);
 
             Groups.player.each(Player::admin, otherPlayer -> {
-                otherPlayer.sendMessage("<[scarlet]A[]>" + prefix + playerName + message);
+                otherPlayer.sendMessage("<[scarlet]A[]>" + prefix + playerName + "[white]> " + message);
             });
         });
         handler.<Player>register("t", "<текст...>", "Отправить командное сообщение", (args, player) -> {
             String message = args[0];
             String playerName = NetClient.colorizeName(player.id, player.name);
-            String prefix = player.admin() ? "[[red]АДМИН[]]" : "[ИГРОК]";
+            String prefix = event.player.admin() ? "[админ]" : "[игрок]";
 
             Groups.player.each(o -> o.team() == player.team(), otherPlayer -> {
-                otherPlayer.sendMessage("<[#" + player.team().color + "]T[]>" + prefix + playerName + message);
+                otherPlayer.sendMessage("<[#" + player.team().color + "]T[]>" + prefix + playerName + "[white]> " + message);
             });
         });
     }
