8447b3eabc54f78ef2f908dfa9db7cb4a503b195
acrobat135 <83962234+acrobat135@users.noreply.github.com>
2021-05-10 16:58:05 +0300

    Update ExamplePlugin.java

--- src/example/ExamplePlugin.java
+++ src/example/ExamplePlugin.java
@@ -1,83 +1,58 @@
 package example;
 
-import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
-import mindustry.game.EventType.*;
-import mindustry.gen.*;
+import arc.*;
+import mindustry.game.*;
 import mindustry.mod.*;
-import mindustry.net.Administration.*;
-import mindustry.world.blocks.storage.*;
+import mindustry.gen.*;
+import mindustry.Vars;
 
 public class ExamplePlugin extends Plugin{
 
     //called when game initializes
     @Override
     public void init(){
-        //listen for a block selection event
-        Events.on(BuildSelectEvent.class, event -> {
-            if(!event.breaking && event.builder != null && event.builder.buildPlan() != null && event.builder.buildPlan().block == Blocks.thoriumReactor && event.builder.isPlayer()){
-                //player is the unit controller
-                Player player = event.builder.getPlayer();
-
-                //send a message to everyone saying that this player has begun building a reactor
-                Call.sendMessage("[scarlet]ALERT![] " + player.name + " has begun building a reactor at " + event.tile.x + ", " + event.tile.y);
+        
+        Events.on(EventType.PlayerChatEvent.class, event -> {
+            if (!event.message.startsWith("/")) {
+                String prefix = player.admin() ? "[[red]АДМИН[]]" : "[ИГРОК]";
+                String playerName = NetClient.colorizeName(event.player.id, event.player.name);
+                Call.sendMessage(prefix + playerName + event.message);
             }
         });
 
-        //add a chat filter that changes the contents of all messages
-        //in this case, all instances of "heck" are censored
-        Vars.netServer.admins.addChatFilter((player, text) -> text.replace("heck", "h*ck"));
+        Vars.netServer.admins.addChatFilter((player, text) -> null);
 
-        //add an action filter for preventing players from doing certain things
-        Vars.netServer.admins.addActionFilter(action -> {
-            //random example: prevent blast compound depositing
-            if(action.type == ActionType.depositItem && action.item == Items.blastCompound && action.tile.block() instanceof CoreBlock){
-                action.player.sendMessage("Example action filter: Prevents players from depositing blast compound into the core.");
-                return false;
-            }
-            return true;
-        });
-    }
-
-    //register commands that run on the server
-    @Override
-    public void registerServerCommands(CommandHandler handler){
-        handler.register("reactors", "List all thorium reactors in the map.", args -> {
-            for(int x = 0; x < Vars.world.width(); x++){
-                for(int y = 0; y < Vars.world.height(); y++){
-                    //loop through and log all found reactors
-                    if(Vars.world.tile(x, y).block() == Blocks.thoriumReactor){
-                        Log.info("Reactor at @, @", x, y);
-                    }
-                }
-            }
-        });
-    }
 
     //register commands that player can invoke in-game
     @Override
     public void registerClientCommands(CommandHandler handler){
-
-        //register a simple reply command
-        handler.<Player>register("reply", "<text...>", "A simple ping command that echoes a player's text.", (args, player) -> {
-            player.sendMessage("You said: [accent] " + args[0]);
-        });
-
-        //register a whisper command which can be used to send other players messages
-        handler.<Player>register("whisper", "<player> <text...>", "Whisper text to another player.", (args, player) -> {
-            //find player by name
-            Player other = Groups.player.find(p -> p.name.equalsIgnoreCase(args[0]));
-
-            //give error message with scarlet-colored text if player isn't found
-            if(other == null){
-                player.sendMessage("[scarlet]No player by that name found!");
+        handler.removeCommand("a");
+        handler.removeCommand("t");
+        handler.<Player>register("a", "<текст...>", "Отправить админское сообщение", (args, player) -> {
+            if (!player.admin) {
+                player.sendMessage("[scarlet]Ты не админ!");
                 return;
             }
 
-            //send the other player a message, using [lightgray] for gray text color and [] to reset color
-            other.sendMessage("[lightgray](whisper) " + player.name + ":[] " + args[1]);
+            String message = args[0];
+            String prefix = "[[red]АДМИН[]]";
+            String playerName = NetClient.colorizeName(player.id, player.name);
+
+            Groups.player.each(Player::admin, otherPlayer -> {
+                otherPlayer.sendMessage("<[scarlet]A[]>" + prefix + playerName + message);
+            });
+        });
+        
+        handler.<Player>register("t", "<текст...>", "Отправить командное сообщение", (args, player) -> {
+            String message = args[0];
+            String playerName = NetClient.colorizeName(player.id, player.name);
+            String prefix = player.admin() ? "[[red]АДМИН[]]" : "[ИГРОК]";
+
+            Groups.player.each(o -> o.team() == player.team(), otherPlayer -> {
+                otherPlayer.sendMessage("<[#" + player.team().color + "]T[]>" + prefix + playerName + message);
+            });
+
         });
     }
 }
